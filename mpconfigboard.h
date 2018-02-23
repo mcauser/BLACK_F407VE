e738e96640c2619fc249518ce1939d4644002c8c
Mike Causer <mcauser@gmail.com>
2018-02-23 18:44:28 +1100

    Update all boards to work with new USB configuration.
    https://github.com/micropython/micropython/commit/34911f1a57296bda532e2460e28bd351198e1e63#diff-4a448575fb7cfab8e70659b57c9cb4cc

--- mpconfigboard.h
+++ mpconfigboard.h
@@ -15,6 +15,7 @@
 #define MICROPY_HW_ENABLE_SERVO     (0)
 #define MICROPY_HW_ENABLE_DAC       (1)
 #define MICROPY_HW_ENABLE_CAN       (1)
+#define MICROPY_HW_ENABLE_USB       (1)
 
 // HSE is 8MHz
 #define MICROPY_HW_CLK_PLLM (8) // divide external clock by this to get 1MHz
@@ -140,5 +141,6 @@
 // 10 SW1 - GND
 
 // USB config
+#define MICROPY_HW_USB_FS (1)
 // #define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
 // #define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
