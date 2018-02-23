14d612b2ae9a1812b15391279b88739fd351c430
Mike Causer <mcauser@gmail.com>
2018-02-23 18:44:40 +1100

    Remove all config options that are set to defaults.
    https://github.com/micropython/micropython/commit/02f88cb2df73b7cf2baf7568c26999269baa45f0#diff-4a448575fb7cfab8e70659b57c9cb4cc

--- mpconfigboard.h
+++ mpconfigboard.h
@@ -6,13 +6,8 @@
 #define MICROPY_HW_HAS_SWITCH       (1)		// has 2 buttons KEY0=PE4, KEY1=PE3
 #define MICROPY_HW_HAS_FLASH        (1)
 #define MICROPY_HW_HAS_SDCARD       (0)		// it has a sd scard, but i am not sure what the detect pin is, yet
-#define MICROPY_HW_HAS_MMA7660      (0)
-#define MICROPY_HW_HAS_LIS3DSH      (0)
-#define MICROPY_HW_HAS_LCD          (0)		// has a ILI9341 TFT connector
 #define MICROPY_HW_ENABLE_RNG       (1)
 #define MICROPY_HW_ENABLE_RTC       (1)
-#define MICROPY_HW_ENABLE_TIMER     (1)
-#define MICROPY_HW_ENABLE_SERVO     (0)
 #define MICROPY_HW_ENABLE_DAC       (1)
 #define MICROPY_HW_ENABLE_CAN       (1)
 #define MICROPY_HW_ENABLE_USB       (1)
